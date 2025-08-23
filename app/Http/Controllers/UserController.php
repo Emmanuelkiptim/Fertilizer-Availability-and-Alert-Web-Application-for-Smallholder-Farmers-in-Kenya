5c322d3e78bc1d758e1e096842975db3a8c38a6b
Emmanuelkiptim <emmanuel.kiptim@strathmore.edu>
2025-08-23 16:41:59 +0300

    added dashboard for roles in the system and managed each person to be directed to their dashboard

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    //
+    public function Dashboard(){
+        if(Auth::check() && Auth::user()->role=='farmer'){
+           return view ('farmer.farmer-dashboard'); 
+        }
+        else if(Auth::check() && Auth::user()->role=='admin'){
+           return view ('admin.admin-dashboard'); 
+        }
+        else if(Auth::check() && Auth::user()->role=='agrovet'){
+           return view ('agrovet.agrovet-dashboard'); 
+        }
+        else{
+            return redirect('/');
+        }
+
+    }
+}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,6 +39,16 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
+        <!-- Role Selection -->
+        <div class="mt-4">
+            <label for="role" class="block text-sm font-medium text-gray-700">Role</label>
+            <select name="role" id="role" class="mt-1 block w-full rounded-md border-gray-300 shadow-sm">
+                <option value="farmer">Farmer</option>
+                <option value="agrovet">Agrovet</option>
+                <option value="admin">Admin</option>
+            </select>
+        </div>
+
         <div class="flex items-center justify-end mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
                 {{ __('Already registered?') }}
